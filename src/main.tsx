4d2af13570fdd47d1ce264e3d5a5737e5a6c6c5f
abdousetif39 <abdousetif940@gmail.com>
2026-08-04 22:26:38 +0100

    Add Vercel Analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
-import {StrictMode} from 'react';
-import {createRoot} from 'react-dom/client';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import './i18n';
+import { Analytics } from "@vercel/analytics/react";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
+    <Analytics />
+  </StrictMode>
 );
